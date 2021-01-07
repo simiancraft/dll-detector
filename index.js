a4f8636837fb0d581adf44de4de83397b973e228
the-simian <harlinjesse@gmail.com>
2021-01-06 23:08:35 -0600

    add the list command

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ let globby = require("globby");
 let fs = require("fs-extra");
 let path = require("path");
 
+const yargs = require("yargs/yargs");
+const { hideBin } = require("yargs/helpers");
+const argv = yargs(hideBin(process.argv)).argv;
+
 const VST_DIR = __dirname;
-const VST_GLOB = "VST/**/*.dll";
+const VST_GLOB = "../../**/*.dll";
 
 const SCAN_GLOB = path
   .resolve(path.join(VST_DIR, VST_GLOB))
   .replace(/\\/g, "/");
+
+console.log(SCAN_GLOB);
 
 function getArchInformation(result) {
   var index = result.indexOf("PE");
@@ -32,8 +38,22 @@ let listFiles = async () => {
     return r;
   });
 
+  function listPlugins(label, plugs) {
+    if (plugs.length) {
+      console.log(`${label} Plugins: ${plugs.length}`);
+      console.table(plugs);
+    }
+  }
+
   Promise.all(tableInfo).then((plugins) => {
-    console.table(plugins);
+    const plug32 = plugins.filter((p) => p.architecture == "32-bit");
+    const plug64 = plugins.filter((p) => p.architecture == "64-bit");
+    const plugUnknown = plugins.filter((p) => p.architecture == "unknown");
+    if (argv.list) {
+      listPlugins("32 Bit", plug32);
+      listPlugins("64 Bit", plug64);
+      listPlugins("??????", plugUnknown);
+    }
   });
 };
 
