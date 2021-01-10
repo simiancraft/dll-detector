d3fc0b03e176da21aef4a2a44d37718dc6149d8b
the-simian <harlinjesse@gmail.com>
2021-01-10 01:22:17 -0600

    reliably skipes certian plugins and also prints a report of all the symlinks it made

--- index.js
+++ index.js
@@ -91,31 +91,35 @@ let listFiles = async () => {
         };
       });
 
-      console.log(`${label} Plugins: ${plugs.length}`);
-      let p = new Table();
-
-      let lastCreator = null;
-      let stripes = ["blue", "magenta", "cyan"];
-      let stripeIndex = -1;
-      let currentColor = null;
-      table.forEach((tr, i) => {
-        if (tr.creator !== lastCreator) {
-          lastCreator = tr.creator;
-          stripeIndex++;
-          if (stripeIndex > stripes.length - 1) {
-            stripeIndex = 0;
-          }
-          currentColor = stripes[stripeIndex];
-        }
-        let color = { color: currentColor };
-        if (tr.match) {
-          color = { color: "yellow" };
-        }
-        let _tr = tr;
-        p.addRow(_tr, color);
-      });
-      p.printTable();
+      printTable(label, table);
     }
+  }
+
+  function printTable(label, table) {
+    console.log(`${label} Plugins: ${table.length}`);
+    let p = new Table();
+
+    let lastCreator = null;
+    let stripes = ["blue", "magenta", "cyan"];
+    let stripeIndex = -1;
+    let currentColor = null;
+    table.forEach((tr, i) => {
+      if (tr.creator !== lastCreator) {
+        lastCreator = tr.creator;
+        stripeIndex++;
+        if (stripeIndex > stripes.length - 1) {
+          stripeIndex = 0;
+        }
+        currentColor = stripes[stripeIndex];
+      }
+      let color = { color: currentColor };
+      if (tr.match) {
+        color = { color: "yellow" };
+      }
+      let _tr = tr;
+      p.addRow(_tr, color);
+    });
+    p.printTable();
   }
 
   function enforceDir(dir) {
@@ -124,7 +128,10 @@ let listFiles = async () => {
     }
   }
 
-  const generateSymlink = (outputDir, report, arch) => (item, index) => {
+  const generateSymlink = (outputDir, report, arch, plugsChecklist) => (
+    item,
+    index
+  ) => {
     //console.log(item.file, outputDir);
     const root = path.resolve(VST_DIR).replace(/\\/g, "/");
     const predicate = item.file.replace(root, "");
@@ -132,18 +139,69 @@ let listFiles = async () => {
       .resolve(path.join(outputDir, predicate))
       .replace(/\\/g, "/");
 
+    //does this plugin have a match?
+    let matchingPlugin = null;
+    if (DEDUPE_ARCH && plugsChecklist && plugsChecklist.length) {
+      matchingPlugin = plugsChecklist.find(possibleMatch(item, root)) || null;
+    }
+
     try {
       enforceDir(path.dirname(newFile));
-      if (!fs.existsSync(newFile)) {
+      if (matchingPlugin) {
+        report.push({ status: "☒ matched", newFile, arch });
+      } else if (!fs.existsSync(newFile)) {
         fs.symlinkSync(item.file, newFile, "file");
-        report.push({ status: "✔️ create", newFile, arch });
+        report.push({ status: "✓ linked", newFile, arch });
+      } else if (fs.existsSync(newFile)) {
+        report.push({ status: "✗ skipped", newFile, arch });
       } else {
-        report.push({ status: "skip", newFile, arch });
+        report.push({ status: "???", newFile, arch });
       }
     } catch (err) {
       console.log(err);
     }
   };
+
+  function printReport(data) {
+    const root = path.resolve(`${VST_DIR}`).replace(/\\/g, "/");
+    let table = data.map((d) => {
+      let _file = d.newFile.replace(root, "");
+      let _dir = path.dirname(_file);
+      return {
+        status: d.status,
+        arch: d.arch,
+        dir: _dir.split("/")[2],
+        creator: _dir.split("/")[3],
+        plug: _dir.split("/").splice(4, 999).join(" | "),
+        name: path.basename(_file),
+      };
+    });
+
+    let lastCreator = null;
+    let stripes = ["blue", "magenta"];
+    let stripeIndex = -1;
+    let currentColor = null;
+    let reportTable = new Table();
+    table.forEach((tr, i) => {
+      if (tr.creator !== lastCreator) {
+        lastCreator = tr.creator;
+        stripeIndex++;
+        if (stripeIndex > stripes.length - 1) {
+          stripeIndex = 0;
+        }
+        currentColor = stripes[stripeIndex];
+      }
+      let color = { color: currentColor };
+      if (tr.status.includes("☒")) {
+        color = { color: "yellow" };
+      } else if (tr.status.includes("✓")) {
+        color = { color: currentColor == stripes[0] ? "cyan" : "green" };
+      }
+      let _tr = tr;
+      reportTable.addRow(_tr, color);
+    });
+    reportTable.printTable();
+  }
 
   const processPlugins = (plugins) => {
     const plug32 = plugins.filter((p) => p.architecture == "32-bit");
@@ -158,9 +216,9 @@ let listFiles = async () => {
 
     if (argv.sortArch) {
       let report = [];
-      plug32.forEach(generateSymlink(VST32Dir, report, "32-bit"));
+      plug32.forEach(generateSymlink(VST32Dir, report, "32-bit", plug64));
       plug64.forEach(generateSymlink(VST64Dir, report, "64-bit"));
-      console.table(report);
+      printReport(report);
     }
   };
 
